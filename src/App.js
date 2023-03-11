03862774849088f518d132eda01128a57f039c06
pavansai <saipt@amazon.com>
2023-03-11 17:41:00 +0530

    Update App.js
    changing in the function

--- src/App.js
+++ src/App.js
@@ -1,55 +1,55 @@
-// import logo from "./logo.svg";
-// import "@aws-amplify/ui-react/styles.css";
-// import {
-//   withAuthenticator,
-//   Button,
-//   Heading,
-//   Image,
-//   View,
-//   Card,
-// } from "@aws-amplify/ui-react";
-
-// import {
-//   ImportExport2 
-//  } from './ui-components';
-
-// function App({ signOut }) {
-//   return (
-//     <View className="App">
-//       <Card>
-//         <Image src={logo} className="App-logo" alt="logo" />
-//         <Heading level={1}>We now have Auth!</Heading>
-//       </Card>
-//       <Button onClick={signOut}>Sign Out</Button>
-//     </View>
-//   );
-// }
-// export default withAuthenticator(App);
-
 import logo from "./logo.svg";
 import "@aws-amplify/ui-react/styles.css";
 import {
   withAuthenticator,
-  // Button,
-  // Heading,
-  // Image,
+  Button,
+  Heading,
+  Image,
   View,
-  // Card,
+  Card,
 } from "@aws-amplify/ui-react";
+
 import {
   ImportExport2 
  } from './ui-components';
- 
+
 function App({ signOut }) {
   return (
     <View className="App">
-      {/* <Card>
+      <Card>
         <Image src={logo} className="App-logo" alt="logo" />
         <Heading level={1}>We now have Auth!</Heading>
       </Card>
-      <Button onClick={signOut}>Sign Out</Button> */}
-      <ImportExport2 />
+      <Button onClick={signOut}>Sign Out</Button>
     </View>
   );
 }
 export default withAuthenticator(App);
+
+// import logo from "./logo.svg";
+// import "@aws-amplify/ui-react/styles.css";
+// import {
+//   withAuthenticator,
+//   // Button,
+//   // Heading,
+//   // Image,
+//   View,
+//   // Card,
+// } from "@aws-amplify/ui-react";
+// import {
+//   ImportExport2 
+//  } from './ui-components';
+ 
+// function App({ signOut }) {
+//   return (
+//     <View className="App">
+//       {/* <Card>
+//         <Image src={logo} className="App-logo" alt="logo" />
+//         <Heading level={1}>We now have Auth!</Heading>
+//       </Card>
+//       <Button onClick={signOut}>Sign Out</Button> */}
+//       <ImportExport2 />
+//     </View>
+//   );
+// }
+// export default withAuthenticator(App);
